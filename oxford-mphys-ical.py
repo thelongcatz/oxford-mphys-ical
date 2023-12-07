0cef3c01fabd87e634dfa553806a13ed5fd90784
thelongcatz <119854517+thelongcatz@users.noreply.github.com>
2023-12-07 20:42:55 +0000

    Code cleanup and added comments

--- oxford-mphys-ical.py
+++ oxford-mphys-ical.py
@@ -15,102 +15,134 @@ s.headers = {
 }
 
 class timetable():
+    """
+    A timetable object that holds ALL relevant information that goes into the final iCalendar file.
+    Since the timetable hosted on the department website does not include any information on the term
+    dates, we have to deduce the actual dates by adding the nth day of the mth week to the date of the
+    first Monday of week 0.
+    The class initialisation would gobble up every single unique entry in the timetable page so
+    tailoring to one's courses should be done afterwards by invoking
+    timetable.calendar.subcomponents.remove().
+    ---
+    INPUT:
+    > term (str): Academic term to query for, i.e. "Michaelmas", "Hilary" or "Trinity"
+    > year (int): The academic year to query for, e.g. for academic year 2023-2024, 2023 would do.
+    > cohort_year (int): Which cohort to query for, e.g. 3 for 3rd years
+    > start_date (datetime.datetime): The date of the first Monday of week 0
+    ---
+    """
     def __init__(self, term, year, cohort_year, start_date):
-        self.start_date = start_date
         self.term = term
         self.year = year
         self.cohort_year = cohort_year
-        self.host = 'https://www3.physics.ox.ac.uk/lectures'
+        self.start_date = start_date
+        self.host = 'https://www3.physics.ox.ac.uk/lectures' # URL of the timetable query page
+        # Translate the English description of each weekday into the corresponding offset from Monday
         self.weekday_lookup = {
-        'Monday': 0,
-        'Tuesday': 1,
-        'Wednesday': 2,
-        'Thursday': 3,
-        'Friday': 4
+            'Monday': 0,
+            'Tuesday': 1,
+            'Wednesday': 2,
+            'Thursday': 3,
+            'Friday': 4
         }
-        self.links = self.link_grabber()
+        # Initialise iCalendar object and add a few attributes to comply with RFC5545
         self.calendar = icalendar.Calendar()
-        self.calendar.add('prodid', '-//Oxford Year {} Physics Timetable {} {}//EN'.format(cohort_year, term, year))
+        self.calendar.add('prodid', f'-//Oxford Year {cohort_year} MPhys Timetable {term} {year}//EN')
         self.calendar.add('version', '2.0')
+        # Create iCalendar events associated with each entry
+        self.links = self.link_grabber()
         for entry in self.links:
             self.create_events(entry['lecture'], entry['url'], self.start_date)
 
-    # Obtain every unique URL of the lectures available
-    # Why not scrap the timetable page, you ask? The afternoon segment complicates the extraction logic
-    # so it's simpler to extract the timetable from each lecture page as per the KISS principle
     def link_grabber(self):
-        url = '{}/timetable.aspx'.format(self.host)
-        params = {'term': self.term, 'year': self.year, 'course': '{}physics'.format(self.cohort_year)}
+        """
+        Obtain every unique URL of the available entries, which is to be scrapped than the timetable page.
+        Why not scrap the timetable page, you ask? The afternoon segment complicates the extraction logic
+        so it's simpler to extract the timetable from each lecture page as per the KISS principle.
+        """
+        url = f'{self.host}/timetable.aspx'
+        params = {'term': self.term, 'year': self.year, 'course': f'{self.cohort_year}physics'}
         page = s.get(url, params=params)
-        assert page.ok, 'HTTP error in fetching timetable!'
+        if not page.ok:
+            raise Exception('HTTP error in fetching timetable!')
         soup = BeautifulSoup(page.content, 'html.parser')
         table = soup.find('table')
         links = []
         for hyperlink in set(table.find_all('a')):
-            links.append({'lecture': hyperlink.get_text(strip=True), 'url': '{}/{}'.format(self.host, hyperlink['href'])})
+            links.append({'lecture': hyperlink.get_text(strip=True), 'url': f'{self.host}/{hyperlink["href"]}'})
         return links
 
-    # Create iCal events based on the information in the lecture page
-    # start_date is the Monday in Week 0
     def create_events(self, lecture, url, start_date):
+        """
+        Create iCalendar events based on the information in a provided course page, i.e. the page you
+        find yourself in upon clicking on one timetable entry.
+        """
         page = s.get(url)
-        assert page.ok, 'HTTP error in fetching lecture information!'
+        if not page.ok:
+            raise Exception('HTTP error in fetching lecture information!')
         soup = BeautifulSoup(page.content, 'html.parser')
-        table = soup.find('table')
-        raw_events = []
-        for row in table.find_all('tr')[1:]:
+        events = []
+        # Skipping the first row since it just contains column titles
+        for row in soup.find('table').find_all('tr')[1:]:
             cells = row.find_all('td')
-            # Check if the term matches - practicals span across different terms!
-            if cells[2].get_text(strip=True) != self.term:
+            # As of 2023 the layout of the table is:
+            # <Weekday> <Week> <Term> <Time slot> <Location>
+            # strip=True gets rid of leading/trailing unwanted whitespace in the texts
+            day, week, term, time_range, location = (cells[i].get_text(strip=True) for i in range(5))
+            # Check if the term matches – practicals span across different terms!
+            if term != self.term:
                 continue
-            day = cells[0].get_text(strip=True)
-            week = int(cells[1].get_text(strip=True))
-            time_range = cells[3].get_text(strip=True)
-            location = cells[4].get_text(strip=True)
+            # The hack here is due to the time string being contaminated by \r, \n, and \t's.
+            # So we first split to remove the whitespaces and group the time into two, before
+            # removing the extra hyphen to make the string valid.
             time_start, time_end = time_range.split()
-            time_start = time_start.replace('-', '')
+            time_start = time_start.replace('-', '').split('.')
+            time_end = time_end.split('.')
             # Construct datetime object for the event
-            day_offset = datetime.timedelta(days=self.weekday_lookup[day], weeks=week)
-            day = start_date + day_offset
-            event_time = []
-            for i in (time_start, time_end):
-                time_string = datetime.datetime.strptime(i, '%H.%M')
-                time_delta = datetime.timedelta(hours=time_string.hour, minutes=time_string.minute)
-                event_time.append(day + time_delta)
-            raw_events.append({'name': lecture, 'start_time': event_time[0], 'end_time': event_time[1], 'location': location})
+            day_delta = start_date + datetime.timedelta(days=self.weekday_lookup[day], weeks=int(week))
+            time_delta_start = datetime.timedelta(hours=int(time_start[0]), minutes=int(time_start[1]))
+            time_delta_end = datetime.timedelta(hours=int(time_end[0]), minutes=int(time_end[1]))
+            events.append({'name': lecture, 'start_time': day_delta + time_delta_start, \
+                            'end_time': day_delta + time_delta_end, 'location': location})
 
-        # Construct event
-        start_time_first = None
-        for index, entry in enumerate(raw_events):
+        # Construct from the list above, this step is required to achieve the automated collating of
+        # adjacent time slots
+        start_time_first = None # Marker to check if the current loop is part of a collation, if not
+                                # then store the value of the first starting time of the chain (if
+                                # identical, adjacent events are found)
+        for index, entry in enumerate(events):
             name = entry['name']
             start_time = entry['start_time']
             end_time = entry['end_time']
             location = entry['location']
-            # Continue if there are identical events along the temporal chain
-            # This code checks if the next entry is identical, then
-            # merge the events into one by discarding intermediate times
+            # Begin collation if there are identical events along the temporal chain
+            # This code checks if the next entry is identical and connects to the current entry,
+            # then merge the events into one by discarding the intermediate time slots
             if index+1 < len(raw_events):
                 next_event = raw_events[index+1]
                 name_next = next_event['name']
                 start_time_next = next_event['start_time']
                 location_next = next_event['location']
-                if start_time_next == end_time and name_next == name and location_next == location:
-                    if not start_time_first:
+                if start_time_next == end_time and \
+                    name_next == name and \
+                    location_next == location:
+                    if start_time_first is None: # i.e. the beginning of the chain
                         start_time_first = start_time
                     continue
-            if start_time_first:
+
+            # Now we finally construct an iCalendar event
+            if start_time_first is not None:
                 start_time = start_time_first
-            event = icalendar.Event()
-            event.add('summary', name)
-            event.add('dtstart', start_time)
-            event.add('dtend', end_time)
-            event.add('dtstamp', datetime.datetime.now(tz=pytz.utc))
-            event.add('location', location)
-            event['uid'] = uuid4()
-            event['URL'] = url
-            start_time_first = None
-            # Add event to calendar
-            self.calendar.add_component(event)
+            ical_event = icalendar.Event()
+            ical_event.add('summary', name)
+            ical_event.add('dtstart', start_time)
+            ical_event.add('dtend', end_time)
+            ical_event.add('dtstamp', datetime.datetime.now(tz=pytz.utc)) # RFC5545 compliance, must be UTC
+            ical_event.add('location', location)
+            ical_event['uid'] = uuid4() # UUID version 4 (random) for uniqueness
+            ical_event['URL'] = url
+            self.calendar.add_component(ical_event) # Add the event into the calendar object
+            start_time_first = None # Reset the marker before the next loop
 
 if __name__ == '__main__':
     def choose_prompt(prompt, choice):
